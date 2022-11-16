9b81943be4be20ba184424e33facc36df348a7d7
Ahmed Hammam <ahmedhammam710@gmail.com>
2022-11-17 00:00:42 +0200

    Added static assets

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #3rd party
+    "crispy_forms",  #new
+    "crispy_bootstrap5",   #new
     #local
     'accounts.apps.AccountsConfig',  #new
     'pages.apps.PagesConfig',  #new
@@ -122,7 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]  #new
+STATIC_ROOT = BASE_DIR / "staticfiles"  #new
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"  #new
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -132,5 +138,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "accounts.CustomUser"   #new
 
 LOGIN_REDIRECT_URL = "home"   #new
-
 LOGOUT_REDIRECT_URL = "home"  #new
+
+#django-crispy-forms
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"   #new
+CRISPY_TEMPLATE_PACK = "bootstrap5"   #new
