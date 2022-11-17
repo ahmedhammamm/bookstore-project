ca1459c9829ec2bde63ac3267690719df2080e34
Ahmed Hammam <ahmedhammam710@gmail.com>
2022-11-17 22:34:46 +0200

    Added allauth, email login, social accounts signing up

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -37,9 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',  #newyy
     #3rd party
     "crispy_forms",  #new
     "crispy_bootstrap5",   #new
+    "allauth",   #new
+    "allauth.account",   #new
+    "allauth.socialaccount",  #newyy
+    "allauth.socialaccount.providers.github",  #newyy
     #local
     'accounts.apps.AccountsConfig',  #new
     'pages.apps.PagesConfig',  #new
@@ -137,9 +142,27 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "accounts.CustomUser"   #new
 
-LOGIN_REDIRECT_URL = "home"   #new
-LOGOUT_REDIRECT_URL = "home"  #new
+#LOGIN_REDIRECT_URL = "home"   #new
+#LOGOUT_REDIRECT_URL = "home"  #new
 
 #django-crispy-forms
+
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"   #new
 CRISPY_TEMPLATE_PACK = "bootstrap5"   #new
+
+#django-allauth config
+
+LOGIN_REDIRECT_URL = "home"
+ACCOUNT_LOGOUT_REDIRECT = "home"   # new
+SITE_ID = 1   #new
+AUTHENTICATION_BACKENDS = (
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",   #new
+)
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"   #new
+ACCOUNT_SESSION_REMEMBER = True  #new
+ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False  #new
+ACCOUNT_USERNAME_REQUIRED = False   #new
+ACCOUNT_AUTHENTICATION_METHOD = "email"   #new
+ACCOUNT_EMAIL_REQUIRED = True   #new
+ACCOUNT_UNIQUE_EMAIL = True   #new
