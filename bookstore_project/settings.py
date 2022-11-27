ae27ded9431e47defb255860a6f12273f2a3e5b9
Ahmed Hammam <ahmedhammam710@gmail.com>
2022-11-27 02:29:42 +0200

    Security configurations for production

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -189,6 +189,15 @@ DEFAULT_FROM_EMAIL = "admin@djangobookstore.com"   #new
 hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())    #new
 INTERNAL_IPS = [ip[:-1] + "1" for ip in ips]       #new
 
+SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
+
+SECURE_HSTS_SECONDS = env.int("DJANGO_SECURE_HSTS_SECONDS", default=2592000)   #30 days
+SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool("DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS", default=True)
+SECURE_HSTS_PRELOAD = env.bool("DJANGO_SECURE_HSTS_PRELOAD", default=True)
+
+SESSION_COOKIE_SECURE = env.bool("DJANGO_SESSION_COOKIE_SECURE", default=True)
+CSRF_COOKIE_SECURE = env.bool("DJANGO_CSRF_COOKIE_SECURE", default=True)
+
 ## email service configration ##
 
 #EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
